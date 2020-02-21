024ab7c8777cba60e753c05c94c727f2bf65e429
Ilya <static.ila@gmail.com>
2020-02-21 20:53:00 +0200

    Add handlers for a few buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,20 +1,43 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Illia Ivanov on 2/21/20.
-//  Copyright © 2020 Illia Ivanov. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var displayNumber: UILabel!
+    
+    var leftOperand = "0"
+    var rightOperand = "0"
+    var operation = ""
+    var isWorkingWithLeftOperandNow = true
+    
+    @IBAction func on9(_ sender: Any) {
+        appendToCurrentOperand(char: "9")
     }
-
-
+    
+    func appendToCurrentOperand(char: String) {
+        if isWorkingWithLeftOperandNow {
+            leftOperand += char
+            displayNumber.text = leftOperand
+        } else {
+            rightOperand += char
+            displayNumber.text = rightOperand
+        }
+    }
+    
+    @IBAction func onMultiply(_ sender: Any) {
+        applyOperation(newOperation: "*")
+    }
+    
+    @IBAction func onResult(_ sender: Any) {
+        let left = Float(leftOperand) ?? 0.0
+        let right = Float(rightOperand) ?? 0.0
+        let result = left * right
+        displayNumber.text = result.description
+        isWorkingWithLeftOperandNow = true;
+    }
+    
+    func applyOperation(newOperation: String) {
+        isWorkingWithLeftOperandNow = false
+        operation = newOperation
+    }
+    
+    func calculateResult() {}
 }
-
